b47990ea70190d1b7f739ce9d882f49ba4be1486
dadav <33197631+dadav@users.noreply.github.com>
2024-01-05 20:41:02 +0100

    feat: add cmd support

--- digipy.py
+++ digipy.py
@@ -30,6 +30,12 @@ class Api:
         if r.text != "OK":
             raise Exception(r.text)
 
+    def cmd(self, command: str):
+        url = f"http://{self._host}:{self._port}/?CMD={command}"
+
+        r = requests.get(url)
+        r.raise_for_status()
+
 
 if len(sys.argv) == 1:
     sys.exit(1)
@@ -38,4 +44,11 @@ api = Api(
     os.environ.get("DIGIPY_HOST", "127.0.0.1"), int(os.environ.get("DIGIPY_PORT", 5513))
 )
 
-api.slc(sys.argv[1], sys.argv[2:])
+if sys.argv[1] == "slc":
+    api.slc(sys.argv[2], sys.argv[3:])
+    sys.exit(0)
+elif sys.argv[1] == "cmd":
+    api.cmd(sys.argv[2])
+else:
+    print("Unknown command")
+    sys.exit(1)
